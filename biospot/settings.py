cfa044dc760a582dd2922a57b49117d6b1bc03fa
diovcharova <di.ovcharova@gmail.com>
2020-02-03 09:49:10 +0000

    added a gitingore file

--- biospot/settings.py
+++ biospot/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'po8x%c9&8=3adw=23_ea7nyf-7l+dj!&t($5z)dcicbz)$_@lu'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
