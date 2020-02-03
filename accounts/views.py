9ce32fc37179a25922d383fe363d6e3542a66571
diovcharova <di.ovcharova@gmail.com>
2020-02-03 14:25:42 +0000

    created a logout view

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
 
 
 def index(request):
-    """
-    Return the index.html file
-    """
+    """Return the index.html file"""
     return render(request, 'index.html')
+
+
+def logout(request):
+    """To logout the user and redirect back to the index page"""
+    auth.logout(request)
+    messages.success(request, 'You have successfully logged out')
+    return redirect(reverse('index'))
