12eaaac26457d1402393d6eb7b6f7e5dfb4b5447
diovcharova <di.ovcharova@gmail.com>
2020-02-03 14:32:27 +0000

    added all apps to the list of installed apps

--- biospot/settings.py
+++ biospot/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'cart',
+    'checkout',
+    'home',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
