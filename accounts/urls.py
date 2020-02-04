a1ab7ef74bed4358b7ae3f078377a446f874e2b9
diovcharova <di.ovcharova@gmail.com>
2020-02-04 12:04:08 +0000

    added a url pattern for password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from . import urls_reset
 from .views import index, register, profile, logout, login
 
 
@@ -8,4 +9,5 @@ urlpatterns = [
     url(r'^profile/$', profile, name='profile'),
     url(r'^logout/$', logout, name='logout'),
     url(r'^login/$', login, name='login'),
+    url(r'^password-reset/', include(urls_reset)),
 ]
