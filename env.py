ff72dc2d7c2ab8827c773ca100b6c0ff77d186fb
diovcharova <di.ovcharova@gmail.com>
2020-02-06 09:29:35 +0000

    added postgres database on heroku

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 
 os.environ.setdefault("SECRET_KEY", 'po8x%c9&8=3adw=23_ea7nyf-7l+dj!&t($5z)dcicbz)$_@lu')
+os.environ.setdefault("DATABASE_URL", 'postgres://xuwibwbldhpejh:c82bb1c59185b7c774e9fe19dd6e00b95094daa9b4b451d50a37a72037826a58@ec2-46-137-177-160.eu-west-1.compute.amazonaws.com:5432/d9avigpld05br6')
