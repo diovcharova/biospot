d4d5719012c2e3c8699ad01f72f2da6166fda60b
diovcharova <di.ovcharova@gmail.com>
2020-02-07 11:29:16 +0000

    added cateogry model

--- products/models.py
+++ products/models.py
@@ -6,13 +6,19 @@ class Tag(models.Model):
     tag = models.CharField(max_length=15)
 
 
+class Category(models.Model):
+    categories = models.ForeignKey('Product', on_delete=models.CASCADE)
+    category = models.CharField(max_length=40)
+
+
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
-    category = models.CharField(max_length=50)
+    categories = models.ManyToManyField(Category, blank=True, symmetrical=False)
     tags = models.ManyToManyField(Tag, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
+    code_internal = models.IntegerField(null=True)
 
     def __str__(self):
         return self.name
