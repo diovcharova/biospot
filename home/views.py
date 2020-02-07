6641208ab1a58913cc404758c6deab6793b1d95e
diovcharova <di.ovcharova@gmail.com>
2020-02-07 09:29:29 +0000

    added the email sending logic for contact form

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ContactForm
+from django.core.mail import EmailMessage
+from django.template.loader import get_template
+from django.contrib import messages
 
 
 def homepage(request):
@@ -10,4 +13,29 @@ def homepage(request):
 def contactspage(request):
     """Return a contact form"""
     form = ContactForm
+    if request.method == 'POST':
+        form = form(data=request.POST)
+        if form.is_valid():
+            contact_name = request.POST.get('contact_name', '')
+            contact_email = request.POST.get('contact_email', '')
+            form_content = request.POST.get('content', '')
+
+            template = get_template('contact_template.txt')
+            context = {
+                'contact_name': contact_name,
+                'contact_email': contact_email,
+                'form_content': form_content,
+            }
+            content = template.render(context)
+
+            email = EmailMessage(
+                "New contact form submission",
+                content,
+                "Your website" + '',
+                ['youremail@gmail.com'],
+                headers={'Reply-To': contact_email}
+            )
+            email.send()
+            messages.success(request, 'Your message was successfully sent!')
+            return redirect('contacts')
     return render(request, 'contacts.html', {'form': form})
