158d96e34c2dcd8c3ca51bfacf8f894e8aabe608
diovcharova <di.ovcharova@gmail.com>
2020-02-10 15:28:48 +0000

    added producer model

--- products/models.py
+++ products/models.py
@@ -2,11 +2,24 @@ from django.db import models
 
 
 class Tag(models.Model):
-    tag = models.CharField(max_length=15)
+    tag = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.tag
 
 
 class Category(models.Model):
     category = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.category
+
+
+class Producer(models.Model):
+    producer = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.producer
 
 
 class Product(models.Model):
@@ -14,8 +27,8 @@ class Product(models.Model):
     categories = models.ManyToManyField(Category, blank=True, symmetrical=False)
     tags = models.ManyToManyField(Tag, blank=True)
     description = models.TextField()
-    package = models.TextField(default='')
-    producer = models.CharField(max_length=254, default='')
+    package = models.CharField(max_length=45,default='')
+    producer = models.ManyToManyField(Producer, blank=True, symmetrical=False)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
     code_internal = models.IntegerField(null=True)
