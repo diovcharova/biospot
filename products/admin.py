97af5296d080970fa0e83cf879275f9678ee8e60
diovcharova <di.ovcharova@gmail.com>
2020-02-10 15:45:34 +0000

    registered producer model on admin

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Product, Tag, Category
+from .models import Product, Tag, Category, Producer
 
 
 admin.site.register(Product)
 admin.site.register(Tag)
 admin.site.register(Category)
+admin.site.register(Producer)
