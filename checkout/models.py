f34af13304f8fb066f606951f1a5f5c25c5fdd92
diovcharova <di.ovcharova@gmail.com>
2020-02-12 10:43:34 +0000

    added email to order model

--- checkout/models.py
+++ checkout/models.py
@@ -12,6 +12,7 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=40, blank=False)
     county = models.CharField(max_length=40, blank=False)
     date = models.DateField()
+    email = models.CharField(max_length=50, blank=False)
 
     def __str__(self):
         return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
