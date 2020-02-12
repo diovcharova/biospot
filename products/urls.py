70bbf13e8318096f0f2bf4fd1e91596eff5cc1ae
diovcharova <di.ovcharova@gmail.com>
2020-02-12 15:17:49 +0000

    added urls for different categories

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import url
-from .views import all_products
+from .views import all_products, products_skincare_and_beauty, products_bulk, products_cleaning_and_household, products_organic, products_supplements, products_raw_and_superfoods
 
 urlpatterns = [
     url(r'^$', all_products, name='products'),
+    url(r'^skincare/$', products_skincare_and_beauty, name='skincare'),
+    url(r'^bulk/$', products_bulk, name='bulk'),
+    url(r'^cleaning/$', products_cleaning_and_household, name='cleaning'),
+    url(r'^organic/$', products_organic, name='organic'),
+    url(r'^supplements/$', products_supplements, name='supplements'),
+    url(r'^raw/$', products_raw_and_superfoods, name='raw'),
+
 ]
