007112db2ecd6a79bc2f692ad7ea49d7bf0102d9
diovcharova <di.ovcharova@gmail.com>
2020-02-14 12:29:39 +0000

    added test for login form

--- accounts/test_forms.py
+++ accounts/test_forms.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class TestUserLogInForm(TestCase):
+
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret'}
+        User.objects.create_user(**self.credentials)
+
+    def test_login(self):
+        response = self.client.post('/login/', self.credentials, follow=True)
+        self.assertTrue(response.context['user'].is_authneticated)
