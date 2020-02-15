5333f32ac806237d8eb65c0348d406168a6ff20e
diovcharova <di.ovcharova@gmail.com>
2020-02-15 11:02:28 +0000

    chabged email settings

--- biospot/settings.py
+++ biospot/settings.py
@@ -175,3 +175,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 EMAIL_PORT = 587
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
