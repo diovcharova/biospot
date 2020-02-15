abee46a4e339cf9a51e68b43f955b8254b6bb7ba
diovcharova <di.ovcharova@gmail.com>
2020-02-15 11:05:45 +0000

    chabged email settings

--- biospot/settings.py
+++ biospot/settings.py
@@ -170,7 +170,7 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
-EMAIL_USE_TSL = True
+EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
