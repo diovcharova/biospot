d9829d831e4dd51e41d443d3981c9dfbf542637b
diovcharova <di.ovcharova@gmail.com>
2020-02-15 10:30:32 +0000

    added tests for views

---
+++ search/test_views.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+
+class TestContactsView(TestCase):
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
