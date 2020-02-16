24854c53c84af13a9711782a61db7bda449e1d93
diovcharova <di.ovcharova@gmail.com>
2020-02-16 13:55:59 +0000

    changed sizes of card on different screens

--- products/views.py
+++ products/views.py
@@ -6,7 +6,7 @@ from django.core.paginator import Paginator
 def all_products(request):
     """Renders a page with all products"""
     products = Product.objects.all()
-    paginator = Paginator(products, 25)
+    paginator = Paginator(products, 24)
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -16,7 +16,7 @@ def all_products(request):
 def products_skincare_and_beauty(request):
     """Renders a page with products from skincare and beauty category"""
     products = Product.objects.filter(categories="1")
-    paginator = Paginator(products, 25)
+    paginator = Paginator(products, 24)
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -26,7 +26,7 @@ def products_skincare_and_beauty(request):
 def products_bulk(request):
     """Renders a page with products from bulk foods category"""
     products = Product.objects.filter(categories="5")
-    paginator = Paginator(products, 25)
+    paginator = Paginator(products, 24)
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -36,7 +36,7 @@ def products_bulk(request):
 def products_cleaning_and_household(request):
     """Renders a page with products from cleaning and household category"""
     products = Product.objects.filter(categories="6")
-    paginator = Paginator(products, 25)
+    paginator = Paginator(products, 24)
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -46,7 +46,7 @@ def products_cleaning_and_household(request):
 def products_organic(request):
     """Renders a page with products from organic foods category"""
     products = Product.objects.filter(categories="4")
-    paginator = Paginator(products, 25)
+    paginator = Paginator(products, 24)
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -56,7 +56,7 @@ def products_organic(request):
 def products_supplements(request):
     """Renders a page with products from supplements category"""
     products = Product.objects.filter(categories="3")
-    paginator = Paginator(products, 25)
+    paginator = Paginator(products, 24)
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -66,7 +66,7 @@ def products_supplements(request):
 def products_raw_and_superfoods(request):
     """Renders a page with products from raw and superfoods category"""
     products = Product.objects.filter(categories="2")
-    paginator = Paginator(products, 25)
+    paginator = Paginator(products, 24)
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
