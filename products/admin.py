759b578899f89d81479e6fbd86310a919b97f5de
diovcharova <di.ovcharova@gmail.com>
2020-02-17 10:44:37 +0000

    removed tags model

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Product, Tag, Category, Producer
+from .models import Product, Category, Producer
 
 
 admin.site.register(Product)
-admin.site.register(Tag)
 admin.site.register(Category)
 admin.site.register(Producer)
--- products/models.py
+++ products/models.py
@@ -1,22 +1,15 @@
 from django.db import models
 
 
-class Tag(models.Model):
-    tag = models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.tag
-
-
 class Category(models.Model):
-    category = models.CharField(max_length=40)
+    category = models.CharField(max_length=40, default="")
 
     def __str__(self):
         return self.category
 
 
 class Producer(models.Model):
-    producer = models.CharField(max_length=40)
+    producer = models.CharField(max_length=40, default="")
 
     def __str__(self):
         return self.producer
@@ -25,11 +18,10 @@ class Producer(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     package = models.CharField(max_length=45, default='', blank=True)
-    categories = models.ManyToManyField(Category, blank=True, symmetrical=False)
+    category = models.ForeignKey(Category, null=True, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField(blank=True)
-    producer = models.ManyToManyField(Producer, blank=True, symmetrical=False)
-    tags = models.ManyToManyField(Tag, blank=True)
+    producer = models.ForeignKey(Producer, null=True, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='images')
     code_internal = models.IntegerField(null=True)
 
