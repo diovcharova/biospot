e77a76a7f150c4c8020cdf77b7c106a78aed0249
diovcharova <di.ovcharova@gmail.com>
2020-02-18 12:04:40 +0000

    checking if input is empty

--- search/views.py
+++ search/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from products.models import Product
+from django.contrib import messages
+from django.core.paginator import Paginator
 
 
 def do_search(request):
-    products = Product.objects.filter(name__icontains=request.GET['q'])
-    return render(request, "products.html", {"products": products})
+    if request.GET['q']:
+        products = Product.objects.filter(name__icontains=request.GET['q'])
+        return render(request, "products.html", {"products": products})
+    else:
+        messages.warning(request, 'Please enter a keyword to search for!')
+        products = Product.objects.all()
+        paginator = Paginator(products, 24)
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'products.html', {'page_obj': page_obj})
