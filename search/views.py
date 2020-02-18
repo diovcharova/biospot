c7c4aaa50bc12e6e52f21cc1afa2993eccb19dd1
diovcharova <di.ovcharova@gmail.com>
2020-02-18 14:14:10 +0000

    empty search returns all products and a message

--- search/views.py
+++ search/views.py
@@ -5,9 +5,13 @@ from django.core.paginator import Paginator
 
 
 def do_search(request):
-    if request.GET['q']:
+    if request.GET['q'] != '':
         products = Product.objects.filter(name__icontains=request.GET['q'])
-        return render(request, "products.html", {"products": products})
+        paginator = Paginator(products, 24)
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'products.html', {'page_obj': page_obj, 'products': products})
     else:
         messages.warning(request, 'Please enter a keyword to search for!')
         products = Product.objects.all()
@@ -15,4 +19,4 @@ def do_search(request):
 
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
-        return render(request, 'products.html', {'page_obj': page_obj})
+        return render(request, 'products.html', {'page_obj': page_obj, 'products': products})
